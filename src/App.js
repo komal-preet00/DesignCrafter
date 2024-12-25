61473c7c6d7efe130a081bd592492c584f96ea4a
ghost-oprator <ambikaghosh008@gmail.com>
2024-12-25 18:36:25 +0530

    wedo dobe

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Navbar from './components/Navbar';
+import WeDo from './components/WeDo';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-      
+      <WeDo />
     </div>
   );
 }
