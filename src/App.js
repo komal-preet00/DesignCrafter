1d5d80b0aa7a88f7f331ca1cfccc7fae804430fe
mehakk34 <pmehak928@gmail.com>
2024-12-25 22:15:54 +0530

    our team

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Navbar from './components/Navbar';
 import Team from './components/Team';
+import WeDo from './components/WeDo';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <WeDo/>
       <Team />
     </div>
   );
